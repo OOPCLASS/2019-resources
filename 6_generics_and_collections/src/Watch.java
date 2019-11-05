563194951f2da2b5b2b28214ecc513095097405d
alexbondor <alexandru.viorel.bondor@gmail.com>
2019-11-05 17:07:45 +0200

    Added Watch.java

---
+++ 6_generics_and_collections/src/Watch.java
@@ -0,0 +1,22 @@
+
+public class Watch {
+
+	private String brandName;
+	private Double price;
+
+	public String getBrandName() {
+		return brandName;
+	}
+
+	public void setBrandName(String brandName) {
+		this.brandName = brandName;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+}
